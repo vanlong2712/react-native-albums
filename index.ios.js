24f2ec063adf6fbcc9a40b606932d91f25e54804
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-10-01 15:08:02 +0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+// Index.ios.js -- place code in here for android!!!
+
+// Import a library to help create a Component
+import { AppRegistry } from 'react-native';
+
+// Create a Component
+import App from './src/App';
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/App.js
@@ -0,0 +1,18 @@
+// Index.ios.js -- place code in here for android!!!
+
+// Import a library to help create a Component
+import React from 'react';
+import { View } from 'react-native';
+// import { Text } from 'react-native';
+import Header from './components/Header';
+import AlbumList from './components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+export default App;
